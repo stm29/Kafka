67490a98416576af64d5052778b9929f201288b6
MohanprasadKumar ST <67147891+stm29@users.noreply.github.com>
2021-05-17 19:30:09 +0530

    Create sending_content_of_file_to_Kafka_as_Message.sh

---
+++ sending_content_of_file_to_Kafka_as_Message.sh
@@ -0,0 +1,2 @@
+#kafka-console-producer.sh --broker-list localhost:9092 --topic Your_topic_here < file_which_is_having_Messages
+ kafka-console-producer.sh --broker-list localhost:9092 --topic registered_user < m
