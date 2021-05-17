c4532ec871a077eb764806a83a7b4ccfbffabf82
MohanprasadKumar ST <67147891+stm29@users.noreply.github.com>
2021-05-17 19:47:17 +0530

    Create topic-create.sh

---
+++ topic-create.sh
@@ -0,0 +1,2 @@
+# bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic topic_name
+  bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic sample
